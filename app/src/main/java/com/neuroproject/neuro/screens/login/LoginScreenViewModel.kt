17609b5ebcc9139f8de98e90b8387d264d1567f0
cascader13 <AlekseiKhitrov@yandex.ru>
2026-02-19 20:55:00 +0300

    initial comment in autorization

--- app/src/main/java/com/neuroproject/neuro/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/neuroproject/neuro/screens/login/LoginScreenViewModel.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
+// буду работать здесь
 @HiltViewModel
 class LoginScreenViewModel @Inject constructor(
     @ApplicationContext private val context: Context
